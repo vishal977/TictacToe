1aef122658e78d7ac253aed64aa0dd49037101f0
Vishal N <vishalnani1999@gmail.com>
2023-02-05 13:19:19 +0530

    Disabling already played squares, and a few optimizations

---
+++ src/Constants.js
@@ -0,0 +1,4 @@
+export const GAME_TITLE = "Tic Tac Toe"
+export const GAMEBOARD_ENABLED = "gameboard"
+export const GAMEBOARD_DISABLED = "gameboardDisabled"
+export const GAME_DRAWN = "It's a draw!"
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,86 +1,90 @@
 import React from 'react'
 import { useState } from 'react'
+import * as Constants from '../Constants'
 
 export default function GameBoard() {
 
-    const [currentPlayer, setCurrentPlayer] = useState("X");
+    const [currentPlayer, setCurrentPlayer] = useState("X")
 
-    const [game, setGame] = useState([["", "", ""], ["", "", ""], ["", "", ""]]);
+    const [game, setGame] = useState([["", "", ""], ["", "", ""], ["", "", ""]])
 
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState("")
 
-    const [gameboardClass, setGameboardClass] = useState("gameboard");
+    const [gameboardClass, setGameboardClass] = useState(Constants.GAMEBOARD_ENABLED)
 
-    const [clicks, setClicks] = useState(0);
+    const [clicks, setClicks] = useState(0)
 
-    function swapPlayer() {
-        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    const swapPlayer = () => {
+        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
     }
 
     const processClick = e => {
-        setClicks(clicks + 1);
-        var id = e.currentTarget.id;
-        let updatedGame = game;
-        updatedGame[Math.floor(id / 10)][id % 10] = currentPlayer;
-        setGame(updatedGame);
-        validateGame();
+        setClicks(clicks + 1)
+        updateGameState(e.currentTarget.id)
+        checkForWin()
+        checkForDraw()
+        swapPlayer()
+    }
+
+    const checkForDraw = () => {
         if (clicks === 8) {
-            setResult("It's a draw!");
-            setClicks(0);
-            setGameboardClass("gameboardDisabled");
+            setResult(Constants.GAME_DRAWN)
+            disableBoard()
         }
-        swapPlayer();
-    };
+    }
 
-    function validateGame() {
-        var set = new Set();
-        set.add(game[0][0].concat(game[0][1]).concat(game[0][2]));
-        set.add(game[1][0].concat(game[1][1]).concat(game[1][2]));
-        set.add(game[2][0].concat(game[2][1]).concat(game[2][2]));
+    const updateGameState = (id) => {
+        game[Math.floor(id / 10)][id % 10] = currentPlayer
+        setGame(game)
+    }
 
-        set.add(game[0][0].concat(game[1][0]).concat(game[2][0]));
-        set.add(game[0][1].concat(game[1][1]).concat(game[2][1]));
-        set.add(game[0][2].concat(game[1][2]).concat(game[2][2]));
+    const declareWinner = (winner) => {
+        setResult(`Player ${winner} wins!`)
+        disableBoard()
+    }
 
-        set.add(game[0][0].concat(game[1][1]).concat(game[2][2]));
-        set.add(game[0][2].concat(game[1][1]).concat(game[2][0]));
+    const disableBoard = () => {
+        setClicks(0)
+        setGameboardClass(Constants.GAMEBOARD_DISABLED)
+    }
 
-        if (currentPlayer === 'X') {
-            if (set.has("XXX")) {
-                setResult("Player X wins!");
-                setClicks(0);
-                setGameboardClass("gameboardDisabled");
-            }
-        } else {
-            if (set.has("OOO")) {
-                setResult("Player O wins!");
-                setClicks(0);
-                setGameboardClass("gameboardDisabled");
-            }
+    const checkForWin = () => {
+        var set = new Set()
+        for (let idx = 0; idx < 3; idx++) {
+            set.add(game[idx][0].concat(game[idx][1]).concat(game[idx][2]))
+            set.add(game[0][idx].concat(game[1][idx]).concat(game[2][idx]))
+        }
+        set.add(game[0][0].concat(game[1][1]).concat(game[2][2]))
+
+        if (currentPlayer === 'X' && set.has(`XXX`)) {
+            declareWinner(`X`)
+        }
+        if (currentPlayer === 'O' && set.has(`OOO`)) {
+            declareWinner(`O`)
         }
 
     }
 
     function resetGame() {
-        setGame([["", "", ""], ["", "", ""], ["", "", ""]]);
-        setResult("");
-        setGameboardClass("gameboard");
-        setCurrentPlayer("X");
+        setGame([["", "", ""], ["", "", ""], ["", "", ""]])
+        setResult("")
+        setGameboardClass(Constants.GAMEBOARD_ENABLED)
+        setCurrentPlayer("X")
     }
 
     return (
         <div className="cont">
             <div className="gameTitle">
-                <h1>Tic Tac Toe</h1>
+                <h1>{Constants.GAME_TITLE}</h1>
             </div>
             <div className="resultText">
                 <h4>{result}</h4>
             </div>
             <div className={gameboardClass} >
                 {
-                    game.map((row, rowIndex) => {
-                        return row.map((col, colIndex) => {
-                            return <div className="box" onClick={ processClick } id = { "" + rowIndex +colIndex }>{col}</div>
+                    game.map((idx, rowIndex) => {
+                        return idx.map((col, colIndex) => {
+                            return <div className={game[rowIndex][colIndex] === "" ? "box" : "boxDisabled"} onClick={processClick} id={"" + rowIndex + colIndex}>{col}</div>
                         })
                     })
                 }
